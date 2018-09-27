d87dfd835ac000ebe4ab014f7877f4383e4dac86
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-09-27 16:31:35 -0700

    updated to dotenv

--- env-sample.sh
+++
@@ -1,2 +0,0 @@
-export REDIS_ENDPOINT='redis://my.redis.host:17952'
-export REDIS_PASSWORD='reDisP@ssw0rd'
--- test/redis.test.js
+++ test/redis.test.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var redis = require("../lib/redis")
   
 describe('Redis', () => {
